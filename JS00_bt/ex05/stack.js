cfcb0f98aae573051a32e70788580b6bd7d86e9f
Btissam Yaqine <btissamyaqine123@gmail.com>
2022-12-28 19:59:01 +0100

    add ex05 from day JS00

--- JS00_bt/ex05/stack.js
+++ JS00_bt/ex05/stack.js
@@ -1,45 +1,52 @@
+ 
+
+
 const stackCreate = () => ({
-  arr: [],
-  // check the arr if is it empty 
-  stackEmpty(stack){
-    if(!Array.isArray(stack)){
-      console.log(0);
-    }
-    if(this.arr.stack.lenght == 0){
-      console.log(0);
-    }else{
-      console.log(1);
-    }
-  },
-  // add a object to the top of an arr
-  stackPush(stack, data){
-    if(!Array.isArray(stack)){
-      console.log(0);
-    } 
-    if(!this.arr.stack.lenght == 0){
-        this.arr.unshift(data);
-    }
-  },
-  // return the data at the top of an array
-  stackPeek(stack){
-      stack = Object.assign({}, this.arr);
-      console.log(stack)
-  },
-  // delete data at the top of an array
-  stackPop(stack){
-  stack = this.arr.shift();
-  console.log(stack)
-  },
+  arr: []
 });
-try {
-    stackCreate.stackEmpty();
-    stackCreate.stackPush();
-    stackCreate.stackPeek();
-    stackCreate.stackPop();
-} catch (error) {
-    stackCreate.stackEmpty() == '';
-    stackCreate.stackPush() == '';
-    stackCreate.stackPeek() == '';
-    stackCreate.stackPop() == '';
-    logMyErrors(error)
+
+// check the arr if is it empty 
+const stackEmpty = (stack) => {
+  if(!Array.isArray(stack))
+    console.log("Stack is Not an Array !");
+  else if (stack.length === 0)
+    console.log("The Array is empty")
+  else 
+    console.log("The Array is not empty")
 }
+// return the data at the top of an array
+ const stackPush = (stack, data) => {
+  stack = [1,2];
+  data = 6;
+    if(!Array.isArray(stack) || stack.length === 0 ){
+      console.log("error check your stack if is it empty or not a table");
+    } else {
+      // const newArray = [data].concat(stack) // true
+      // const newArray = stack.unshift([data])] // it's not true it's show the number of index
+      const newArray = [data, ...stack] // true
+        console.log(newArray);
+    }
+  };
+  // returns the data at the top of an array in an object (stack)
+  const stackPeek = (stack) => {
+      stack = [1,2,3]
+      if(stack.length !== 0){
+      const myObject = Object.assign({}, stack);
+      console.log(myObject);
+    }else console.log("error")
+  };
+    // delete data at the top of an array
+  const stackPop = (stack) => {
+    stack = [1,2,3,4]
+    if (stack.length === 0){
+      console.log("error")
+    }else{
+    const myPop = stack.shift();
+    console.log(myPop)
+    console.log(stack)
+  }
+  }
+// stackEmpty([5]);
+// stackPush([])
+// stackPeek()
+stackPop()
