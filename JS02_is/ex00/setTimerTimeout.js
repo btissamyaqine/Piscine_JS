f10309fcef798276b44f30e7f20409ffbd3d45df
issamelferkh <issam.elferkh@gmail.com>
2023-01-04 23:42:19 +0100

    JS02/ex00

--- JS02_is/ex00/setTimerTimeout.js
+++ JS02_is/ex00/setTimerTimeout.js
@@ -1,22 +1,56 @@
+// // setTimer must call the callback function before being stopped by callback at every interval after the input time interval.
+// //  Never call callback immediately after setTimer has run!
 
-interval = () => {
+
+
+// count : number
+// callback(count) //returns true/false => to stop or continue the setTimer
+// interval: var | () ???
+// setTimer(callback, interval)
+//   - call 
+//     -> callback() 
+//       -> passi 
+//         -> count (each call +1)
+
+
+
+
+// setTimer(setTimeout,setInterval) {
+//   while setTimeout -> true
+//   ...
+// }
+
+// setTimeout(count) -> true/false
+
+// setInterval() {
+//   return 100;
+// }
+// -> interval : 100
+
+
+
+
+
+
+
+
+setInterval  = () => {
   return (2);
 }
 
-callback = (count) => {
+setTimeout  = (count) => {
   if (count)
     return (true) //returns true/false => to stop or continue the setTimer
   return (false) //returns true/false => to stop or continue the setTimer
 }
 
-setTimer = (callback, interval) => {
-  inc = interval();
+setTimer = (setTimeout , setInterval ) => {
+  inc = setInterval();
   while (inc) {
-    console.log(callback(interval()));
+    console.log(setTimeout);
     inc--;
   }
   // callback(interval());
 }
 
-
-setTimer(callback, interval);
+setTimer()
