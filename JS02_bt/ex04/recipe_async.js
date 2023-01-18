2db38dfcf51ba0fd09e1da044f331bed8ac2586a
Btissam Yaqine <btissamyaqine123@gmail.com>
2023-01-18 12:54:07 +0100

    update ex04 day JS02

--- JS02_bt/ex04/recipe_async.js
+++ JS02_bt/ex04/recipe_async.js
@@ -6,15 +6,58 @@ function randomFail() {
 }
 async function restaurant_food() {
 
-  let make_dough = new Promise((resolve, reject) => {
+  let make_dough =  new Promise((resolve) => {
     try{
-      setTimeout(() => resolve("make dought is done!"), 3000)
+       setTimeout(() => resolve("make dought is done!"), 3000)
 
     } catch {
       randomFail()
     }
   });
   let resault = await make_dough
-  console.log(resault)
+  console.log(resault);
+
+  let primary_fermentation =  new Promise((resolve) => {
+    try{
+       setTimeout(() => resolve("primary fermentation are done!"), 5000)
+
+    } catch {
+      randomFail()
+    }
+  });
+  let resault1 = await primary_fermentation
+  console.log(resault1);
+
+  let molding =  new Promise((resolve) => {
+    try{
+       setTimeout(() => resolve("Molding are done!"), 4200)
+
+    } catch {
+      randomFail()
+    }
+  });
+  let resault2 = await molding
+  console.log(resault2);
+  
+
+  let moldSecond_fermentationing = new Promise((resolve) => {
+    try {
+      setTimeout(() => resolve("Molding fermentationing are done"), 2000)
+    } catch {
+      randomFail()
+    }
+  })
+  let result3 = await moldSecond_fermentationing;
+  console.log(result3);
+
+  let fry = new Promise((resolve) => {
+    try {
+      setTimeout(() => resolve("Fry is done!"), 5000)
+    } catch {
+      randomFail();      
+    }
+  })
+  let result4 = await fry;
+  console.log(result4)
 }
-restaurant_food()
+restaurant_food();
