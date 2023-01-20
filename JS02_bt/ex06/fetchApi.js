dfcdb801faa1d13f342ca72e89a9e0552b5b330d
Btissam Yaqine <btissamyaqine123@gmail.com>
2023-01-20 19:00:46 +0100

    update day 2

---
+++ JS02_bt/ex06/fetchApi.js
@@ -0,0 +1,20 @@
+
+// import axios from 'axios'
+import fetch from 'cross-fetch';
+Promise.all([
+	fetch('https://jsonplaceholder.typicode.com/comments?postId=9'),
+	fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+]).then(function (responses) {
+	// Get a JSON object from each of the responses
+	return Promise.all(responses.map(function (response) {
+		return response.json();
+	}));
+}).then(function (data) {
+   data;
+	// Log the data to the console
+	// You would do something with both sets of data here
+	console.log(data);
+}).catch(function (error) {
+	// if there's an error, log it
+	console.log(error);
+});
