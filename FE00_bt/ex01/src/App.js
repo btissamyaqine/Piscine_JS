c938db844ddc83dfcd6248d79bfc4ebb974be502
Btissam Yaqine <btissamyaqine123@gmail.com>
2023-01-31 15:07:54 +0100

    add delete and add to ex01 day FE and i have isseu in update state

--- FE00_bt/ex01/src/App.js
+++ FE00_bt/ex01/src/App.js
@@ -4,38 +4,84 @@ import ToDoListTemplate from './ToDoListTemplate';
 function App() {
   const [inputtxt, setInputtxt] = React.useState("");
   const [todo, setTodo] = React.useState([]);
+  const [editForm, setEditForm] = React.useState(false);
+  const [id, setId] = React.useState();
   const handelTextInput = (e) =>{
     console.log(e.target.value)
     setInputtxt(e.target.value);
   }
 const submitFormHandler = (e) => {
-  e.preventDefault()
+  e.preventDefault();
+  if(!inputtxt) return;
   setTodo([
-    ...todo, {text: inputtxt, id: Math.random}
+    ...todo, {text: inputtxt, id: Math.random() * 99}
   ])
   setInputtxt("");
+};
+const editFormHandler = (e) => {
+  e.preventDefault();
+  let items = [...todo];
+  let item = items[id];
+  item.inputtxt = inputtxt;
+  items[id] = item;
+  setTodo(items);
+  setInputtxt('');
+  setEditForm(false);
 }
 
   return (
-    <div className="App">
+    <>
+      {editForm===false&&(
+      <div className="App">
       <h1>ToDoList</h1>
-      <div onSubmit={submitFormHandler} className="enter">
-        {/* <h5>Part to entrer </h5> */}
+      <form onSubmit={submitFormHandler} className="enter">
+        <h5>Part to entrer </h5>
         <input value={inputtxt} onChange={handelTextInput} type="text" />
-      </div>
+      </form>
       <h5>ToDo List Part</h5>
+      {todo.length>0 && (
       <ul>
-      {todo.map(todos => (
-      <ToDoListTemplate
-      todos={todos}
-      todo={todo}
-      setTodo={setTodo} 
-      text={todos.text}
-      key={todos.id}
-      />
-      ))}
-      </ul>
+          {todo.map(todos => (
+          <ToDoListTemplate
+          todos={todos}
+          todo={todo}
+          setTodo={setTodo} 
+          text={todos.text}
+          key={todos.id}
+          />
+          ))}
+        </ul>
+    )}
     </div>
+    )}
+    
+    
+    {editForm===true&&(
+      <div className="App">
+        <h1>ToDoList</h1>
+        <form onSubmit={editFormHandler} className="enter">
+          <h5>Part to entrer </h5>
+          <input value={inputtxt} onChange={handelTextInput} type="text" />
+        </form>
+        {todo.length>0 && (
+      <ul>
+          {todo.map(todos => (
+          <ToDoListTemplate
+          todos={todos}
+          todo={todo}
+          setTodo={setTodo} 
+          text={todos.text}
+          key={todos.id}
+          />
+          ))}
+        </ul>
+    )}
+      </div>
+    )}
+    
+    
+    </>
+    
   );
 }
 
