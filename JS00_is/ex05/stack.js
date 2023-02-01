aefa1e859cf50417bc5c8222cce142ac7d8c449c
issamelferkh <issam.elferkh@gmail.com>
2023-02-01 18:23:31 +0100

    JS00 ex05: in progress

--- JS00_is/ex05/stack.js
+++ JS00_is/ex05/stack.js
@@ -2,48 +2,39 @@
 // 2. execute tests
 
 // - stackEmpty(stack) : checks whether an array in an object (stack) is empty. Returns a Boolean.
-// - stackPush(stack, data) : A function that adds data to the top of an array in an object (stack).
-// - stackPeek(stack) : A function that returns the data at the top of an array in an object (stack).
-// - stackPop(stack) : A function that deletes the data at the top of an array in an object (stack).
+// - stackPush(stack, data) : adds    data to the top of an array in an object (stack).
+// - stackPeek(stack) :       returns data at the top of an array in an object (stack).
+// - stackPop(stack) :        deletes data at the top of an array in an object (stack).
 
 const stackCreate = () => ({
   arr: []
 });
 
-// check the arr if is it empty 
 const stackEmpty = (stack) => {
-  if(!Array.isArray(stack))
-    console.log("Is Not a Stack !");
-  else if (stack.length === 0)
-    console.log(1)
-  else 
-    console.log(0)
+  return JSON.stringify(stack) === '{"arr":[]}';
 }
 
-stackEmpty([2]);
+const stackPush = (stack, data) => {
+  stack = Object.assign(data, stack)
+  return stack
+}
+
+// Object.values(obj).sort((a,b) =>b-a).slice(0,5);
+
+const stackPeek = (stack) => {
+  return (stack[Object.keys(stack)[Object.keys(stack).length-1]]);
+}
+
+const stackPop = (stack) => {
+  stack = this.arr.shift();
+  return stack
+},
 
 
-  
-//   // add a object to the top of an arr
-//   stackPush(stack, data){
-//     if(!Array.isArray(stack)){
-//       console.log(0);
-//     } 
-//     if(!this.arr.stack.lenght == 0){
-//         this.arr.unshift(data);
-//     }
-//   },
-
-//   // return the data at the top of an array
-//   stackPeek(stack){
-//       stack = Object.assign({}, this.arr);
-//       console.log(stack)
-//   },
-//   // delete data at the top of an array
-//   stackPop(stack){
-//   stack = this.arr.shift();
-//   console.log(stack)
-//   },
-// });
+stack = stackCreate();
+// console.log(stackEmpty(stack))
+console.log(stackPush(stack, [73,7]))
+// console.log(stackPeek(stack))
+stackPop(stack)
 
 
