7bd6db06fccfdc8ef07189af2c8d67dee43aebd3
Btissam Yaqine <btissamyaqine123@gmail.com>
2023-03-14 14:23:14 +0100

    update ex00 day FE02

--- FE02_bt/ex00/src/App.js
+++ FE02_bt/ex00/src/App.js
@@ -3,18 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button
-        type="button"
-        onClick={() => {
-          fetch("http://localhost:3000/")
-            .then((response) => console.log(response.json()))
-            .then((payload) => {
-              console.log(payload)
-            });
-        }}
-      >
-        Fetch List
-      </button>
+      
     </div>
   );
 }
