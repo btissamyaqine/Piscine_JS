a2dd0268515eb474a1e45679d1111e5a85aecefe
Btissam Yaqine <btissamyaqine123@gmail.com>
2023-03-15 19:27:22 +0100

    add ex03 day FE02

--- FE02_bt/ex02/src/App.js
+++ FE02_bt/ex02/src/App.js
@@ -1,9 +1,47 @@
+import React,{ useState, useEffect} from 'react';
+// import fetch from 'node-fetch';
 import './App.css';
-import './../post/data.json';
+// import datas from './post/data';
+let datas = require('./post/data')
 function App() {
+  const [data, setData] = useState(datas);
+
+  const fetchJson = () => {
+    fetch('datas')
+    .then(response => {
+      return response.json();
+    }).then(data => {
+      setData(data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  // fetchJson();
+    useEffect(() => {
+    fetchJson()
+    },[])
   return (
     <div className="App">
-      
+    
+    {/* <p>{datas.post_adress}</p> */}
+    {data && data.posts.map((item, id) => (
+        <p key={id}>{item.post_title}</p>
+      ))}
+      ---------------------------------
+      {/* <p>{datas.post_adress}</p> */}
+      {data && data.posts.map((item, id) => (
+        <p key={id}>{item.post_adress}</p>
+      ))}
+      ----------------------------------
+      {/* <p>{datas.post_adress}</p> */}
+      {data && data.posts.map((item, id) => (
+        <p key={id}>{item.post_adress}</p>
+      ))}
+      -----------------------------------
+      {/* <p>{datas.post_adress}</p> */}
+      {data && data.posts.map((item, id) => (
+        <p key={id}>{item.Created_time}</p>
+      ))}
     </div>
   );
 }
