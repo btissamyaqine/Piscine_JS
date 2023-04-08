e5d058325ca1de3aa45eb71da672ed3610cb6f7a
Btissam Yaqine <btissamyaqine123@gmail.com>
2023-04-08 16:58:05 +0100

    update ex02

--- FE02_bt/ex02/server.js
+++ FE02_bt/ex02/server.js
@@ -1,5 +1,21 @@
-const corsProxy = require('cors-anywhere');
-const PORT = process.env.PORT || 3001;
-corsProxy.createServer().listen(PORT, () => {
-  console.log(`CORS Anywhere server running on port ${PORT}`);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- FE02_bt/ex02/src/App.js
+++ FE02_bt/ex02/src/App.js
@@ -1,30 +1,3 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { NotionRenderer } from 'react-notion';
-import 'react-notion/src/styles.css';
 
-export default   function App() {
-  const [data1, setData1] = useState({});
-  useEffect(() =>  {
-//     const response =  fetch('http://localhost:3001/https://api.notion.com/v1/pages/85e24e498ab3495fa684c187310501a9', {
-//   headers: {
-//     'Authorization': 'Bearer secret_yhprlTWcor8hjo5GwADQe5W6OxQlaCWpuFmRgeDW8RU',
-//     'Notion-Version': '2021-08-16' // specify the Notion API version you want to use
-//   }
-// })
-fetch(
-  'api.notion.com/v1/pages/85e24e498ab3495fa684c187310501a9'
-)
-      .then((res) => res.json())
-      .then((data1) => setData1(data1));
-  }, []);
-
-
-  return (
-    <div>
-    <NotionRenderer blockMap={data1} />
-    </div>
-  )
-}
-  
+// import React from 'react'
 
