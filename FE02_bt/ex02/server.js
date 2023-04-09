020482a0e6572a9562a0be5b449b56e0bc69e3a3
Btissam Yaqine <btissamyaqine123@gmail.com>
2023-04-09 19:51:32 +0100

    update ex02 day FE02 - now i can add a page to notion api from input

--- FE02_bt/ex02/server.js
+++ FE02_bt/ex02/server.js
@@ -1,21 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv')
+const { Client } = require('@notionhq/client');
 
+const app = express();
+app.use(cors());
+app.use(express.json())
+dotenv.config();
 
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
+app.post('/create-page', async (req, res) => {
+  const { title } = req.body;
 
+  try {
+    const response = await notion.pages.create({
+      parent: {
+        database_id: process.env.NOTION_DATABASE_ID,
+      },
+      "properties": {
+        "title": {
+          "title": [
+            {
+              "text": {
+                "content": title,
+              },
+            },
+          ],
+        },
+        
+      },
+    });
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Something went wrong');
+  }
+});
+app.get('/get_page', async(req, res) => {
+      try { 
+          const response = await notion.databases.query({
+              database_id: process.env.NOTION_DATABASE_ID, 
+          });
+  
+          res.json(response);
+        } catch (error) {
+          console.error(error);
+          res.status(500).send('Something went wrong');
+        }
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- FE02_bt/ex02/src/App.js
+++ FE02_bt/ex02/src/App.js
@@ -1,3 +1,70 @@
+import React from 'react'
+import axios from 'axios';
 
-// import React from 'react'
+function App() {
+  const [title, setTitle] = React.useState('');
+   const [APIData, setAPIData] = React.useState([]);
+  const createPage = async (e) => {
+  try {
+    const response = await axios.post('http://localhost:5000/create-page', {
+      title,
+    });
 
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    const response = await axios.get('http://localhost:8000/get_page')
+      .then(response => {
+        setAPIData(response.data.results);
+        console.log(response.data.results);
+      })
+  } catch (error) {
+    console.error(error);
+  }
+
+  };
+  // createPage(title)
+  return (
+        <div className=''>
+    
+    <form className='' onSubmit={createPage}>
+      <h1 className=''>React Out To Us</h1>
+      <div>
+        <label htmlFor="name">Full Name</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="John Doe"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+      </div>
+      
+      <button className="" type="submit">
+        Submit
+      </button>
+    </form>
+    <div>
+    <p>API DATA</p>
+          {
+            APIData.map((data) => {
+              return (
+                <div key={data.id}>
+                   <p>Name: {data.properties.title.title[0].text.content}</p>
+                   
+               </div>
+              )
+           })
+          }
+    </div>
+  </div>
+  )
+  }
+
+
+
+export default App
